bbb9ddd5f3bfaa846f798f6f13dad4e664eb308f
shirouxx <summerdream0715@gmail.com>
2018-09-27 23:19:49 +0900

    Finish Layout and routes

--- test/helpers/application_test_helper.rb
+++ test/helpers/application_test_helper.rb
@@ -4,5 +4,6 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title," Ruby on Rails Tutoial"
     assert_equal full_title("Help"),"Help | Ruby on Rails Tutoial"
+    assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutorial"
   end
 end
